31d7b695717c1c1f81130edb15ddb98ed1bffe7c
Gun Daniel Lee 이건 <gdlee412@gmail.com>
2023-05-08 19:54:00 +0900

    P4

--- 과제/p04/main.c
+++ 과제/p04/main.c
@@ -5,63 +5,63 @@
 
 int main(int argc, char* argv[])
 {
-    char filename[] = "Aladdin_num.txt";
-    int STDIN, STDOUT;
-    int line_num = 1;
-    char num_to_string[50];
-    char input_char;
-    char line_number_format[] = " | ";
+	char filename[] = "Aladdin_num.txt";
+	int STDIN, STDOUT;
+	int line_num = 1;
+	char num_to_string[50];
+	char input_char;
+	char line_number_format[] = " | ";
 
-    if(argc != 2)
-    {
-            perror("required file name");
-            exit(1);
-    }
+	if(argc != 2)
+	{
+		perror("required file name");
+		exit(1);
+	}
 
-    if((STDIN = open(argv[1], O_RDONLY, 0755)) < 0 || (STDOUT = open(filename, O_RDWR | O_CREAT, 0755)) < 0)
-    {
-            perror("open");
-            exit(1);
-    }
+	if((STDIN = open(argv[1], O_RDONLY, 0755)) < 0 || (STDOUT = open(filename, O_RDWR | O_CREAT, 0755)) < 0)
+	{
+		perror("open");
+		exit(1);
+	}
 
-    num_to_string[0] = '0' + line_num;
-    write(STDOUT, num_to_string, 1);   
-    write(STDOUT, line_number_format, sizeof(line_number_format) - 1);
-    line_num++;
+	num_to_string[0] = '0' + line_num;
+	write(STDOUT, num_to_string, 1);
+	write(STDOUT, line_number_format, sizeof(line_number_format) - 1);
+	line_num++;
 
-    while(read(STDIN, &input_char, 1) != 0)
-    {
-        write(STDOUT, &input_char, 1);
-        if(input_char == '\n')
-        {
-            int digit_count = 0;
-            int temp_num = line_num;
-            while(temp_num != 0)
-            {
-                    temp_num /= 10;
-                    digit_count++;
-            }
+	while(read(STDIN, &input_char, 1) != 0)
+	{
+		write(STDOUT, &input_char, 1);
+		if(input_char == '\n')
+		{
+			int digit_count = 0;
+			int temp_num = line_num;
+			while(temp_num != 0)
+			{
+				temp_num /= 10;
+				digit_count++;
+			}
 
-            temp_num = line_num;
-            for(int i = digit_count - 1; i >= 0; i--)
-            {
-                    num_to_string[i] = '0' + temp_num % 10;
-                    temp_num /= 10;
-            }
+			temp_num = line_num;
+			for(int i = digit_count - 1; i >= 0; i--)
+			{
+				num_to_string[i] = '0' + temp_num % 10;
+				temp_num /= 10;
+			}
 
-            num_to_string[digit_count] = '\0';
+			num_to_string[digit_count] = '\0';
 
-            write(STDOUT, num_to_string, digit_count);
-            write(STDOUT, line_number_format, sizeof(line_number_format) - 1);
-            line_num++;
-        }
-    }
+			write(STDOUT, num_to_string, digit_count);
+			write(STDOUT, line_number_format, sizeof(line_number_format) - 1);
+			line_num++;
+		}
+	}
+	
+	if(close(STDIN) < 0 || close(STDOUT) < 0)
+	{
+		perror("close");
+		exit(1);
+	}
 
-    if(close(STDIN) < 0 || close(STDOUT) < 0)
-    {
-        perror("close");
-        exit(1);
-    }
-
-    exit(0);
+	exit(0);
 }
